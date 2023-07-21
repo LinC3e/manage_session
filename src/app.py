5392d8c76ca6128a3c19b418690ddb37079e7043
LinC3e <ivancostilla17@gmail.com>
2023-07-21 19:00:07 -0300

    TEST - request data from login test

--- src/app.py
+++ src/app.py
@@ -1,15 +1,22 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request , redirect, url_for
 from config import config
+import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    db.conectar()
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    if request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
